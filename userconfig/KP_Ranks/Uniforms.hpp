67ee5fd6b9c93dce9db93cc32d324eed28e89dea
By-Jokese <byjokese@gmail.com>
2017-09-23 02:45:24 +0200

    Auto stash before revert of "Debug Version!!! 1 user + 1 rank"

--- userconfig/KP_Ranks/Uniforms.hpp
+++ userconfig/KP_Ranks/Uniforms.hpp
@@ -8,4 +8,7 @@ KP_Ranks_uniforms =
 	["MCOE_Uniform_Veterano_MC",0]
 ];
 
+publicVariable "FSG_Ranks_uniforms";
+
 publicVariable "KP_Ranks_uniforms";
+>>>>>>> parent of 9336e5e... Merge pull request #1 from byjokese/MCOE
