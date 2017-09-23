44aed43ae6639bbc4fa7dec00fb7a09af20545e3
Kile467 <luismartin467@hotmail.com>
2017-09-23 02:41:24 +0200

    Update Uniforms.hpp

--- userconfig/KP_Ranks/Uniforms.hpp
+++ userconfig/KP_Ranks/Uniforms.hpp
@@ -5,7 +5,25 @@ KP_Ranks_uniforms =
 	// Factions:
 	// 0 = MCOE
 	// 1 = FSG
-	["MCOE_Uniform_Veterano_MC",0]
+	["MCOE_Uniform_Veterano_MC",0],
+	["MCOE_Uniform_Veterano_MC_Arido",0],
+	["MCOE_Uniform_Veterano_MC_Tropical",0],
+	["MCOE_Uniform_Veterano_MC_Artico",0],
+	["MCOE_Uniform_Veterano_MC_Urbano",0],
+	["MCOE_Uniform_Veterano_MC_Negro",0],
+	["MCOE_Uniform_Infanteria_MC",0],
+	["MCOE_Uniform_Infanteria_MC_Arido",0],
+	["MCOE_Uniform_Infanteria_MC_Tropical",0],
+	["MCOE_Uniform_Infanteria_MC_Artico",0],
+	["MCOE_Uniform_Infanteria_MC_Urbano",0],
+	["MCOE_Uniform_Infanteria_MC_Negro",0],
+	["MCOE_Uniform_Infanteria_MC_Arremangado",0],
+	["MCOE_Uniform_Infanteria_MC_Arido_Arremangado",0],
+	["MCOE_Uniform_Infanteria_MC_Tropical_Arremangado",0],
+	["MCOE_Uniform_Infanteria_MC_Artico_Arremangado",0],
+	["MCOE_Uniform_Infanteria_MC_Urbano_Arremangado",0],
+	["MCOE_Uniform_Infanteria_MC_Negro_Arremangado",0],
+	
 ];
 
 publicVariable "KP_Ranks_uniforms";
