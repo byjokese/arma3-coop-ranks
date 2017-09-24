a6969a508016610ad6d1ed8fe8a6737208dc7832
By-Jokese <byjokese@gmail.com>
2017-09-24 18:55:48 +0200

    After merge

--- FSG_Ranks/functions/kp_ranks_loadServerconfig.sqf
+++
@@ -1,4 +0,0 @@
-#include "\userconfig\KP_Ranks\Ranks.hpp"
-#include "\userconfig\KP_Ranks\Uniforms.hpp"
-
-diag_log "[KP RANKS] Dedicated server initialised";
--- userconfig/KP_Ranks/Uniforms.hpp
+++ userconfig/KP_Ranks/Uniforms.hpp
@@ -23,25 +23,7 @@ KP_Ranks_uniforms =
 	["MCOE_Uniform_Infanteria_MC_Tropical_Arremangado",0],
 	["MCOE_Uniform_Infanteria_MC_Artico_Arremangado",0],
 	["MCOE_Uniform_Infanteria_MC_Urbano_Arremangado",0],
-	["MCOE_Uniform_Infanteria_MC_Negro_Arremangado",0],
-// FSG
-	["FSG_Custom_Camo_Nieve",1],
-	["FSG_Custom_Camo_Nieve_Mix",1],
-	["FSG_Custom_Camo_Nieve_White",1],
-	["FSG_Custom_Camo_marpat",1],
-	["FSG_Custom_Camo_marpat1",1],
-	["FSG_Custom_Camo_marpat2",1],
-	["FSG_Custom_Camo_marpat_marron",1],
-	["FSG_Custom_Boscoso",1],
-	["FSG_Custom_Boscoso_Arremangado",1],
-	["FSG_Custom_Boscoso_Corto",1],
-	["FSG_Custom_Boscoso_Gris",1],
-	["FSG_Custom_Camo_m81",1],
-	["FSG_Custom_Camo_m81_verde",1],
-	["FSG_Custom_Camo_aor1",1],
-	["FSG_Custom_Camo_aor1_green",1],
-	["FSG_sniper_Camo_marpat",1]
-
+	["MCOE_Uniform_Infanteria_MC_Negro_Arremangado",0]
 ];
 
 publicVariable "KP_Ranks_uniforms";
